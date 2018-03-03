591b07d156e125bba689ab179002b6ab81d44a14
Nekliukov <__>
2018-03-03 15:31:24 +0300

    strings completed 15/15

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -273,7 +273,10 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    if (typeof value == 'string' || value instanceof String)
+        return true;
+    else
+        return false;
 }
 
 
@@ -308,10 +311,7 @@ function getCardId(value) {
             'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
             'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠' 
           ];
-    for (var i=0; i<cards.length; i++){
-        if (cards[i] == value)
-            return i;
-    }
+    return cards.indexOf(value);
 }
 
 
