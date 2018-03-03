19432dfd685c46e8bf033aa6c6184e06a73e5355
Nekliukov <__>
2018-03-03 17:03:26 +0300

    Revert "Numbers: 10/12 completed. Some problems with returning of an avarage of two numbers, doesn't work"
    This reverts commit 38ce485ae81aab770596a579f5540ddf91da3886.

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -55,7 +55,6 @@ function getCicleCircumference(radius) {
  */
 function getAverage(value1, value2) {
     throw new Error('Not implemented');
-    //return (value1+value2)/2;
 }
 
 /**
@@ -74,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    return Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+    return Math.sqrt(Math.pow(2,Math.abs(x1-x2)) + Math.pow(2,Math.abs(y1-y2))) ;
 }
 
 /**
@@ -90,12 +89,12 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-    return (-b/a)? -b/a : 0;
+    throw new Error('Not implemented');
 }
 
 
 /**
- * Возвращает   угол (в радианах) между двумя векторами, заданными значениями xi и yi в декартовой системе координат
+ * Возвращает угол (в радианах) между двумя векторами, заданными значениями xi и yi в декартовой системе координат
  * Подробнее https://en.wikipedia.org/wiki/Euclidean_vector#Representations
  *
  * @param {number} x1
@@ -112,9 +111,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    var numerator = x1*x2 + y1*y2, //2
-        divisior  = Math.sqrt(x1*x1+y1*y1)*Math.sqrt(x2*x2+y2*y2); //1 + sqrt5
-    return Math.acos(numerator/divisior);
+    throw new Error('Not implemented');
 }
 
 /**
@@ -130,7 +127,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-    return Math.abs(value % 10);
+    var num=value.toString;
+    return num[0];
 }
 
 
@@ -146,7 +144,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-    return Number(value);
+    throw new Error('Not implemented');
 }
 
 /**
@@ -163,7 +161,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a, b, c) {
-    return Math.sqrt(a*a + b*b + c*c);
+    throw new Error('Not implemented');
 }
 
 /**
@@ -205,11 +203,7 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    for(var i=2; i<n; i++){
-        if (n % i == 0)
-            return false;
-    }
-    return true;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -228,7 +222,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    return (Number(value))? Number(value): def;
+    throw new Error('Not implemented');
 }
 
 module.exports = {
